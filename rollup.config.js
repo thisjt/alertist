a04156e5ec021a88cf0d30a95b165f2122119f63
Joshua Tahanlangit <contact@thisjt.me>
2024-06-14 08:01:28 +0800

    chore: create a minified and unminified version of dist

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,14 @@ export default [
 			name: 'alertist',
 			file: 'dist/alertist.js',
 			format: 'esm',
+		},
+	},
+	{
+		input: 'src/js/main.js',
+		output: {
+			name: 'alertist',
+			file: 'dist/alertist.min.js',
+			format: 'esm',
 			compact: true,
 			plugins: [terser()],
 		},
@@ -17,6 +25,14 @@ export default [
 			name: 'alertist',
 			file: 'dist/alertist.browser.js',
 			format: 'iife',
+		},
+	},
+	{
+		input: 'src/js/main.js',
+		output: {
+			name: 'alertist',
+			file: 'dist/alertist.browser.min.js',
+			format: 'iife',
 			compact: true,
 			plugins: [terser()],
 		},
@@ -25,6 +41,13 @@ export default [
 		input: 'src/js/main.js',
 		output: {
 			file: 'dist/alertist.cjs',
+			format: 'cjs',
+		},
+	},
+	{
+		input: 'src/js/main.js',
+		output: {
+			file: 'dist/alertist.min.cjs',
 			format: 'cjs',
 			compact: true,
 			plugins: [terser()],
