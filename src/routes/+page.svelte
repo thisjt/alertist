624444f8cbaffd6d1d727e201f32da0b6547a614
Joshua Tahanlangit <contact@thisjt.me>
2024-06-17 12:48:48 +0800

    feat: toast actions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -96,10 +96,22 @@
 	<div>Toast Examples</div>
 	<div>
 		<button
-			on:click={() => {
-				alertist.toast({ text: `Hello! The current time is ${new Date().toLocaleString()}.` });
+			on:click={async () => {
+				const action = await alertist.toast({ text: `Hello! The current time is ${new Date().toLocaleString()}.` });
+				alertist.alert({ text: `Toast closed by ${action ? 'user' : 'timeout'}!` });
 			}}>Basic</button>
-		<button>Click to Dismiss</button>
+		<button
+			on:click={() => {
+				alertist.toast({ text: `Hello! The current time is ${new Date().toLocaleString()}.`, closebutton: true });
+			}}>With X Button</button>
+		<button
+			on:click={() => {
+				alertist.toast({ text: `Hello! The current time is ${new Date().toLocaleString()}.`, timeout: 1000 });
+			}}>1 Second Timeout</button>
+		<button
+			on:click={() => {
+				alertist.toast({ text: `Hello! The current time is ${new Date().toLocaleString()}.`, closebutton: true, closeonclick: false });
+			}}>No Close on Click</button>
 	</div>
 </div>
 
