202a51f59f1c1259ca858e38b3af8584211deecb
Joshua Tahanlangit <contact@thisjt.me>
2024-06-22 11:52:56 +0800

    test: util full code coverage

--- src/lib/fakedom.js
+++ src/lib/fakedom.js
@@ -1,13 +1,18 @@
+import { vi } from 'vitest';
 import jsdom from 'jsdom';
 const JSDOM = jsdom.JSDOM;
 
-export function dom() {
+export default function dom() {
 	const dom = new JSDOM('<!DOCTYPE html><html lang="en"></html>');
 
 	// No support for HTMLDialogElement yet
 	// https://github.com/jsdom/jsdom/issues/3294
 	dom.window.HTMLDialogElement.prototype.showModal = () => {};
 	dom.window.HTMLDialogElement.prototype.close = () => {};
+	dom.window.HTMLElement.prototype.showPopover = () => {};
+
+	vi.stubGlobal('document', dom.window.document);
+	vi.stubGlobal('DOMParser', dom.window.DOMParser);
 
 	return dom;
 }
--- src/lib/util.test.js
+++ src/lib/util.test.js
@@ -1,9 +1,8 @@
-import { expect, test, vi } from 'vitest';
-import { dom } from './fakedom';
-vi.stubGlobal('document', dom().window.document);
-vi.stubGlobal('DOMParser', dom().window.DOMParser);
+import { expect, test } from 'vitest';
+import dom from './fakedom';
+dom();
 
-import { alertistBucket, alertistToastBucket, alertistStringToHtml } from './util';
+import { alertistBucket, alertistToastBucket, alertistStringToHtml, alertistInit, alertistCleanup, alertistRandomString, alertistButtons } from './util';
 
 test('alertistBucket is null', () => {
 	expect(alertistBucket).toBe(null);
@@ -16,4 +15,29 @@ test('alertistToastBucket is null', () => {
 test('alertistStringToHtml', () => {
 	const html = alertistStringToHtml('<div>test</div>');
 	expect(html.outerHTML).toBe('<div>test</div>');
+});
+
+test('alertistRandomString', () => {
+	expect(alertistRandomString().length).toBeGreaterThan(0);
+});
+
+test('alertistButtons', () => {
+	expect(alertistButtons.close).toContain('data:image/png;base64');
+});
+
+test('alertistCleanup before init', () => {
+	expect(alertistCleanup()).toBe(undefined);
+});
+
+test('alertistInit', () => {
+	expect(alertistInit()).toBe(true);
+});
+
+test('alertistCleanup after init', () => {
+	expect(alertistCleanup()).toBe(undefined);
+});
+
+test('alertistInit outside document context', () => {
+	global.document = undefined;
+	expect(alertistInit()).toBe(null);
 });
